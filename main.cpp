c89ded1844927f326ffaddd4db9cb896e0c72496
MorganReyS <morgan.rey@etu.u-bordeaux.fr>
2026-01-21 16:58:30 +0100

    class WarehouseInstance

--- main.cpp
+++ main.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+class WarehouseInstance {
+
+    public:
+
+    vector<vector<int>> adjacency;
+    vector<int> rank_capacity;
+    vector<int> product_circuit;
+    vector<vector<int>> aisles_racks;
+    vector<int> orders;
+
+    int num_racks;
+    int total_slots;
+    int aeration_rate;
+    int num_products;
+    int num_circuits;
+    int num_aisles;
+    int num_orders;
+
+};
+
 int main(int argc, char** argv) {
     cout << "blabla" << endl;
 
